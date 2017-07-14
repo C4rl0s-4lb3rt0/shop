f0b55b3b435c8bcb01d6117d01502224f8554003
Carlos <carlos@mexicoshowroom.com.mx>
2017-07-14 08:49:24 -0500

    cambios en el Beat

--- js/app.js
+++ js/app.js
@@ -35,3 +35,21 @@ function mostrarModal(){
     localStorage.noMostrarModal = true
   })
 }
+
+
+const $filtrosToggle = $('#filtrosToggle')
+$filtrosToggle.click( function (ev){
+  ev.preventDefault()
+
+  const $i =  $filtrosToggle.find('i.fa')
+// buscar esa elemento i con la clase i.fa
+
+  const isDown = $i.hasClass('fa-chevron-down')
+// lo gueardamos en una variable preguntamos si tiene la clase fa-chevron-down
+
+  if(isDown){
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  }else{
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
